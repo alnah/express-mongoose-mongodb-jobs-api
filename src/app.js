d5783456f4eddfa3942cd71c7be9a31fe1716a32
alnah <alexis.nahan@gmail.com>
2024-07-19 08:33:45 -0300

    feat(app): use authentication and job routes

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
 require("dotenv").config();
 require("express-async-errors");
-
 const express = require("express");
 
-// const routesPlaceholder = require("./controllers/routesPlaceholder");
+const authRoutes = require("./routes/auth");
+const jobsRoutes = require("./routes/jobs");
 const routeNotFoundMiddleware = require("./middlewares/route-not-found");
 const errorHandlerMiddleware = require("./middlewares/error-handler");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// parser
 app.use(express.json());
 
-// app.use("/api/v1/", routesPlaceholder);
+// routes
+app.use("/api/v1", authRoutes);
+app.use("/api/v1", jobsRoutes);
+
+// middlewares
 app.use(routeNotFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
+// start
 app.listen(port, () => console.log(`Server is listening on port ${port}...`));
