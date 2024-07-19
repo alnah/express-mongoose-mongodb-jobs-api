c74ff5f77bc454af91df8a6e12324fc0fb7c76a8
alnah <alexis.nahan@gmail.com>
2024-07-19 12:50:28 -0300

    refactor(register): use pre-save password hashing hook

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,14 +1,9 @@
-const bcrypt = require("bcryptjs");
 const { StatusCodes } = require("http-status-codes");
 
 const User = require("../models/user");
 
 const register = async (req, res, next) => {
-  const { name, email, password } = req.body;
-  const salt = await bcrypt.genSalt();
-  const hashedPassword = await bcrypt.hash(password, salt);
-  const tempUser = { name, email, password: hashedPassword };
-  const user = await User.create({ ...tempUser });
+  const user = await User.create({ ...req.body });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
