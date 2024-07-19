b645b9a1953c9cfd95a8a760743a7f65f7974b1a
alnah <alexis.nahan@gmail.com>
2024-07-19 12:37:43 -0300

    feat(register): hash user password during registration

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,15 @@
+const bcrypt = require("bcryptjs");
+const { StatusCodes } = require("http-status-codes");
+
+const User = require("../models/user");
+
 const register = async (req, res, next) => {
-  res.json({ postman: "register" });
+  const { name, email, password } = req.body;
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const tempUser = { name, email, password: hashedPassword };
+  const user = await User.create({ ...tempUser });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res, next) => {
