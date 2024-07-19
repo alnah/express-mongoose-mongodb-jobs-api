a4d4e5ac74fc1430464d032875c34002e6fabc6b
alnah <alexis.nahan@gmail.com>
2024-07-19 12:29:33 -0300

    fix(UserSchema): replace express-validator by validator package

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const { isEmail } = require("express-validator");
+const { isEmail } = require("validator");
 
 const UserSchema = new mongoose.Schema({
   name: {
