c03f9fa24fa54bcb5bd0c4f7c3314abc8b5e60cb
alnah <alexis.nahan@gmail.com>
2024-07-19 13:05:37 -0300

    feat(register): generate JWT

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,10 +1,19 @@
+require("dotenv").config();
 const { StatusCodes } = require("http-status-codes");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
 const register = async (req, res, next) => {
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = jwt.sign(
+    { userId: user._id, name: user.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "30d",
+    }
+  );
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name, token } });
 };
 
 const login = async (req, res, next) => {
