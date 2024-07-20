08de3784a16cf5990a6dc006888d012420ec5ce6
alnah <alexis.nahan@gmail.com>
2024-07-20 15:14:44 -0300

    feat(error-handler): add validation error logic

--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -6,10 +6,16 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   // set default
   const customError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-    message: err.message || "Something went wrong try again later",
+    message: err.message || "Something went wrong try again later.",
   };
   if (err.code && err.code === 11000) {
-    customError.message = `Duplicate value entered ${Object.keys(err.keyValue)} field, please choose another value`;
+    customError.message = `Duplicate value entered ${Object.keys(err.keyValue)} field, please choose another value.`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+  if (err.name === "ValidationError") {
+    customError.message = Object.values(err.errors)
+      .map((e) => e.message)
+      .join(" ");
     customError.statusCode = StatusCodes.BAD_REQUEST;
   }
   return res
